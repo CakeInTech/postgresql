893f34148ed3af81e3d339da0a1454d274620613
MohamedCK <abdulhakimm280@gmail.com>
2022-12-12 22:03:03 +0200

    Revert "write down the schema of the table"
    This reverts commit 8ae46144ea7ad2f8e2829db1a6506442185d80fd.

--- schema.sql
+++ schema.sql
@@ -1,17 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
--- CREATE TABLE animals (
---     name varchar(100)
--- );
-
-CREATE TABLE animals
-(
-    id integer NOT NULL,
-    name character varying(100) COLLATE pg_catalog."default",
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg numeric,
-    CONSTRAINT animals_pkey PRIMARY KEY (id)
+CREATE TABLE animals (
+    name varchar(100)
 );
-
