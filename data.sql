da4f7da63fd16886a0e7c64b0e08627350144866
MohamedCK <abdulhakimm280@gmail.com>
2022-12-12 22:12:34 +0200

    create animal table db

--- data.sql
+++ data.sql
@@ -3,3 +3,9 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals VALUES 
+(1, 'Agumon', '03-02-2020', 0, true, 10.23),
+(2, 'Gabumon', '11-15-2018', 2, true, 8), 
+(3, 'Pikachu', '01-07-2021', 1, false, 15.04), 
+(4, 'Devimon', '05-12-2017', 5, true, 11);
