ee6cdd81200f0e5641c50db00dbe1ba821403421
MohamedCK <abdulhakimm280@gmail.com>
2022-12-13 21:28:35 +0200

    - Write queries to answer questions.

--- queries.sql
+++ queries.sql
@@ -37,3 +37,10 @@ ROLLBACK TO S1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts), neutered FROM animals GROUP BY neutered;
+SELECT Min(weight_kg), Max(weight_kg), COUNT(weight_kg), species FROM animals GROUP BY species;
+SELECT AVG(escape_attempts), species, date_of_birth FROM animals GROUP BY species, date_of_birth HAVING date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
