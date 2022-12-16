eefd9ffa7893399ae5ad8e5feb3dec4880c33cf0
MohamedCK <abdulhakimm280@gmail.com>
2022-12-16 12:50:18 +0200

    Update the schema to include the new tables

--- schema.sql
+++ schema.sql
@@ -43,3 +43,22 @@ DROP COLUMN species;
 ALTER TABLE animals ADD species_id BIGINt REFERENCES species(id);
 
 ALTER TABLE animals ADD owners_id BIGINT REFERENCES owners(id);
+
+
+CREATE TABLE specialization (
+    vets_id BIGINT REFERENCES vets (id),
+    species_id BIGINT REFERENCES species (id)
+);
+
+CREATE TABLE vets (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    age INT,
+    date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE visits (
+    vets_id BIGINT REFERENCES vets (id),
+    animals_id BIGINT REFERENCES animals (id),
+    date_of_visit DATE NOT NULL
+);
