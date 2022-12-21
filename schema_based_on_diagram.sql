f6c59a7e1804acc6422f4ea1a3ab644d6ebd2350
MohamedCK <abdulhakimm280@gmail.com>
2022-12-21 16:36:52 +0200

    - make changes according to the review

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,16 +27,23 @@ CREATE TABLE invoice_items (
   total_price DECIMAL NOT NULL,
   invoices_id INT NULL REFERENCES invoices(id) ON DELETE CASCADE,
   treatment_id INT NULL REFERENCES treatment(id) ON DELETE CASCADE
-)
+);
 
-CREATE TABLE tretments (
+CREATE TABLE treatments (
   id BIGSERIAL NOT NULL PRIMARY KEY, 
   type VARCHAR(40) NOT NULL,
   name VARCHAR(40) NOT NULL
-)
+);
+
+CREATE TABLE medical_historeis_treatment ( 
+ medical_history_id INT REFERENCES medical_historeis(id) ON DELETE CASCADE,
+ treatment_id INT REFERENCES treatment(id) ON DELETE CASCADE
+);
 
 -- Index Clauses 
 CREATE INDEX patient_id ON patients(id);
 CREATE INDEX medical_history_id ON medical_historeis(id);
 CREATE INDEX invoices_id ON invoices(id);
+CREATE INDEX treatment_id ON treatment(id);
+CREATE INDEX invoice_items ON invoices(id);
 CREATE INDEX treatment_id ON treatment(id);
